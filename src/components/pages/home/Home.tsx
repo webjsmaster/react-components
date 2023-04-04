e055602e4af0896af586028817b83c0762b9d48f
web-js-master <web.js.master@gmail.com>
2023-04-04 22:30:46 +0300

    feat:fetch all

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,27 +1,47 @@
-import React from 'react';
-import scss from './Home.module.scss';
-import { cards, ICardHome } from '../../../utils/mockData';
+import React, { FC, useEffect, useState } from 'react';
 import { Layout } from '../../ui/layout/Layout';
+import PreLoader from '../../ui/preloader/PreLoader';
 import Card from './card/Card';
+import scss from './Home.module.scss';
 
-function Home() {
+const Home: FC = () => {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch('https://dummyjson.com/products?limit=0&skip=0')
+      .then((res) => res.json())
+      .then((data) => {
+        setIsLoading(false);
+        setProducts(data.products);
+      });
+  }, []);
+
+  console.log('📌:', products);
+
   return (
     <Layout>
-      <nav className={scss.wrapper} data-testid="home-page">
-        {cards.map((t: ICardHome) => (
-          <Card
-            key={t.id}
-            id={t.id}
-            name={t.name}
-            description={t.description}
-            poster={t.poster}
-            like={t.like}
-            view={t.view}
-          />
-        ))}
-      </nav>
+      {isLoading ? (
+        <PreLoader />
+      ) : (
+        <nav className={scss.wrapper} data-testid="home-page">
+          {products &&
+            products.map(({ id, title, description, thumbnail, price, rating }) => (
+              <Card
+                key={id}
+                id={id}
+                name={title}
+                description={description}
+                poster={thumbnail}
+                like={price}
+                view={rating}
+              />
+            ))}
+        </nav>
+      )}
     </Layout>
   );
-}
+};
 
 export default Home;
